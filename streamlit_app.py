bfad5f4fc0cb853c9e108bae64c48edb4303dc60
NetaJerusalem <avrahama@neta-project.org>
2022-12-27 17:19:31 +0200

    some changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,6 @@ import pandas as pd
 import streamlit as st
 from Utilities import WriteData
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 st.title("Welcome to Neta streamlit project")
 
 import streamlit as st
@@ -44,5 +34,5 @@ if external_name:
 st.dataframe(df)
 st.subheader("playground")
 with st.echo(code_location='below'):
-    pass
+    st.button("playground")
     
