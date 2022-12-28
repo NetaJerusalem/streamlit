8a26f91640a0be514ec2f91dbd6414e2cc1531f4
NetaJerusalem <avrahama@neta-project.org>
2022-12-28 21:45:22 +0200

    change home page

--- Home.py
+++ Home.py
@@ -17,7 +17,7 @@ st.image("https://media.giphy.com/media/scZPhLqaVOM1qG4lT9/giphy.gif")
 st.code(os.fspath("pages_\\data\\status.csv"))
 st.code(garret_burhenn_pitches_csv)
 path = st.text_input("enter path")
+st.code(__file__)
 buuton = st.button("load form path")
 if buuton:
     st.dataframe(pd.read_csv(path))
-    st.code(__file__)
