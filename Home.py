ce7fa39d3f6e7ad560cc8234638f92f8a6b239ee
NetaJerusalem <avrahama@neta-project.org>
2022-12-28 16:49:50 +0200

    change main page

--- Home.py
+++ Home.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import os
 # from pages.data.Utilities import WriteAnswer
 
 st.set_page_config(
@@ -11,3 +12,8 @@ st.title("Welcome to Neta Senionr Exercises")
 
 st.write("Please select the required exercise on the left")
 st.image("https://media.giphy.com/media/scZPhLqaVOM1qG4lT9/giphy.gif")
+st.code(os.fspath("pages_\\data\\status.csv"))
+path = st.text_input("enter path")
+buuton = st.button("load form path")
+if buuton:
+    st.dataframe(pd.read_csv(path))
