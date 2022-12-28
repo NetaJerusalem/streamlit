293074891aa739ce787183d757acb9ab25e945b8
NetaJerusalem <avrahama@neta-project.org>
2022-12-28 17:31:27 +0200

    update home

--- Home.py
+++ Home.py
@@ -2,7 +2,9 @@ import pandas as pd
 import streamlit as st
 import os
 # from pages.data.Utilities import WriteAnswer
+from pathlib import Path
 
+garret_burhenn_pitches_csv = Path(__file__).parents[1] / 'GarretBurhennData/Garret_Burhenn_Pitches.csv'
 st.set_page_config(
     page_title="Hello",
     page_icon="👋",
@@ -13,6 +15,7 @@ st.title("Welcome to Neta Senionr Exercises")
 st.write("Please select the required exercise on the left")
 st.image("https://media.giphy.com/media/scZPhLqaVOM1qG4lT9/giphy.gif")
 st.code(os.fspath("pages_\\data\\status.csv"))
+st.code(garret_burhenn_pitches_csv)
 path = st.text_input("enter path")
 buuton = st.button("load form path")
 if buuton:
