9cc334c170d92694a2f884672df428665be74c19
NetaJerusalem <avrahama@neta-project.org>
2023-01-18 18:29:48 +0200

    change to no bar

--- pages/QuickQues.py
+++ pages/QuickQues.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from pages.Utilities.Utilities import Questions, Utilities
 from pathlib import Path
-st.set_page_config(page_title="Quick Questions",
-                   page_icon="🔢")
 
-Questions.quick_questions("quick_code_ques.py",num_questoin=1)
+st.set_page_config(page_title="Quick Questions", page_icon="🔢",initial_sidebar_state="collapsed")
+
+Questions.quick_questions_no_bar("quick_ques_function.py", num_questoin=1)
