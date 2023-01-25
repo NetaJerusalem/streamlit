a9e5ac8fb9ba07c3617bc018b937f752d188022b
NetaJerusalem <avrahama@neta-project.org>
2023-01-26 00:47:28 +0200

    add exercise for junior

--- pages/QuickQues.py
+++ pages/QuickQues.py
@@ -4,4 +4,4 @@ from pathlib import Path
 
 st.set_page_config(page_title="Quick Questions", page_icon="🔢",initial_sidebar_state="collapsed")
 
-Questions.quick_questions_no_bar("quick_ques_function.py", num_questoin=1)
+Questions.quick_questions_no_bar("quick_code_ques.py", num_questoin=1)
