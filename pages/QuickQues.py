71e14a437de64cc7fdf1ec138e96e45eadd1f3f4
NetaJerusalem <avrahama@neta-project.org>
2023-02-16 10:31:41 +0200

    move old pages

--- pages/QuickQues.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from pages.Utilities.Utilities import Questions, Utilities
-from pathlib import Path
-
-st.set_page_config(page_title="Quick Questions", page_icon="🔢",initial_sidebar_state="collapsed")
-
-Questions.quick_questions_no_bar("quick_code_ques.py", num_questoin=1)
