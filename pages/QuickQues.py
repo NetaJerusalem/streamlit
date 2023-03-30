f357c56a1a7b3d17cba8e89ee851684ca42ef8b3
NetaJerusalem <avrahama@neta-project.org>
2023-03-30 12:20:46 +0300

    rearranging the library

--- pages/QuickQues.py
+++ pages/QuickQues.py
@@ -3,5 +3,5 @@ from __init__ import Questions, Utilities, QuickQuestions
 from pathlib import Path
 
 st.set_page_config(page_title="Quick Questions", page_icon="🔢",initial_sidebar_state="collapsed")
-QuickQuestions("quick_ques_function.py",seconds=3, num_questoin=1)
+QuickQuestions("cods_questions/quick_ques_function.py",seconds=3, num_questoin=1)
 
