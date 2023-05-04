532ca14b45bd8e9967338134daa8d540df1a48e5
NetaJerusalem <avrahama@neta-project.org>
2023-05-04 15:24:38 +0300

    add QuickQuestionsNoBar

--- pages/QuickQues.py
+++ pages/QuickQues.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from __init__ import Questions, Utilities, QuickQuestions
+from __init__ import Questions, Utilities, QuickQuestions ,QuickQuestionsNoBar
 from pathlib import Path
 
 st.set_page_config(page_title="Quick Questions", page_icon="🔢",initial_sidebar_state="collapsed")
-QuickQuestions("cods_questions/quick_ques_function.py", num_questoin=1)
+QuickQuestionsNoBar("cods_questions/slicing.py", num_questoin=1)
 
